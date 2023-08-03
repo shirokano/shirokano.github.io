230d10a81596d9912d34697b1201a0d0470ac39c
shirokano <okshiro915@gmail.com>
2023-08-03 11:50:20 -0700

    test

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
+          {tempVar}
         </p>
         <a
           className="App-link"
