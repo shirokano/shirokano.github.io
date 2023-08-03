92115558ba93d4794b4c8b2b9f3a5e5770f5bf54
shirokano <okshiro915@gmail.com>
2023-08-03 19:29:14 +0000

    Automated changes

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const tempVar = 6
+  const tempVar = 6;
   return (
     <div className="App">
       <header className="App-header">
