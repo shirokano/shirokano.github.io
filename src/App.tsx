dde174d89e1be333e10fa27461d2fb20ed4b1196
shirokano <okshiro915@gmail.com>
2023-08-03 12:45:24 -0700

    remove temp var

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const tempVar = 6;
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
-          {tempVar}
         </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           learn react
         </a>
       </header>
