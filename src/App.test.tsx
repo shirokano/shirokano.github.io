7f99ebf3d48ee561fe222de226a4ab076015ac9a
shirokano <okshiro915@gmail.com>
2023-09-04 10:52:49 -0700

    update test-id

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const helloWorld = screen.getByTestId('hello-world');
+  const helloWorld = screen.getByTestId('experience');
   expect(helloWorld).toBeInTheDocument();
 });
