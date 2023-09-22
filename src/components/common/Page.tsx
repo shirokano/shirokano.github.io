0a259a4a14a78cddd936fb2774943db3840b9a74
shirokano <okshiro915@gmail.com>
2023-09-22 15:29:54 -0700

    add Page component & Context

--- src/components/common/Page.tsx
+++ src/components/common/Page.tsx
@@ -1,15 +1,44 @@
-import React, { ReactElement } from 'react';
+import React, { useContext, useEffect, useRef, useState } from 'react';
+import { PageContext } from './PageContext';
 import './Page.scss';
 
-interface PageProps {
-  children: ReactElement;
+interface PageProviderProps {
+  children: React.ReactNode;
 }
 
-function Page({ children }: PageProps) {
+function Page({ children }: PageProviderProps) {
+  const pageRef = useRef<HTMLDivElement>(null);
+  const [isPageAtTop, setIsPageAtTop] = useState<boolean>(false);
+
+  useEffect(() => {
+    const pageRefDiv = pageRef.current;
+    const observer = new IntersectionObserver(
+      (entries, _) => {
+        const entry = entries[0];
+        setIsPageAtTop(entry.isIntersecting);
+      },
+      { threshold: 1 }
+    );
+
+    /* eslint-disable no-debugger */
+    // debugger;
+    if (pageRefDiv) {
+      observer.observe(pageRefDiv);
+    }
+
+    return () => {
+      if (pageRefDiv) {
+        observer.unobserve(pageRefDiv);
+      }
+    };
+  }, [isPageAtTop]);
+
   return (
-    <>
-      <div className="page">{children}</div>
-    </>
+    <PageContext.Provider value={[isPageAtTop]}>
+      <div ref={pageRef} className="page">
+        {children}
+      </div>
+    </PageContext.Provider>
   );
 }
 
