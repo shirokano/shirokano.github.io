266ddfa7f944eb6073490706ba8c96c2d6d2820c
shirokano <okshiro915@gmail.com>
2023-09-22 15:43:36 -0700

    🔨 fix test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+jest.mock('./components/common/Page.tsx', () => () => {
+  return <div data-testid="page" />;
+});
+
 test('renders learn react link', () => {
   render(<App />);
   const menuId = screen.getByTestId('main');
